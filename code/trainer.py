4ca77bee2245ee29187c17b8c6acbbd6a6164d72
s-52 <behrens.soenke@web.de>
2021-05-15 20:24:47 +0000

    trainer.py error message edit

--- code/trainer.py
+++ code/trainer.py
@@ -469,76 +469,80 @@ class condGANTrainer(object):
             netG.eval()
             #print("eval")
             for key in data_dic:
-                print(f"key: {key}")
-                save_dir = '%s/%s' % (s_tmp, key)
-                mkdir_p(save_dir)
-                captions, cap_lens, sorted_indices = data_dic[key]
+                try:
+                    print(f"key: {key}")
+                    save_dir = '%s/%s' % (s_tmp, key)
+                    mkdir_p(save_dir)
+                    captions, cap_lens, sorted_indices = data_dic[key]
 
-                batch_size = captions.shape[0]
-                #print(f"batch size: {batch_size}")
-                nz = cfg.GAN.Z_DIM
+                    batch_size = captions.shape[0]
+                    #print(f"batch size: {batch_size}")
+                    nz = cfg.GAN.Z_DIM
 
-                with torch.no_grad():
-                    captions = Variable(torch.from_numpy(captions))
-                    cap_lens = Variable(torch.from_numpy(cap_lens))
-
-                    captions = captions.cuda()
-                    cap_lens = cap_lens.cuda()
-
-                for i in range(1):  # 16
-                    #print(f"i: {i}")
                     with torch.no_grad():
-                        noise = Variable(torch.FloatTensor(batch_size, nz))
-                        noise = noise.cuda()
-                    #print("noise done")
-                    #######################################################
-                    # (1) Extract text embeddings
-                    ######################################################
-                    hidden = text_encoder.init_hidden(batch_size)
-                    #print("textencode1")
-                    # words_embs: batch_size x nef x seq_len
-                    # sent_emb: batch_size x nef
-                    words_embs, sent_emb = text_encoder(captions, cap_lens, hidden)
-                    #print("textencode2")
-                    mask = (captions == 0)
-                    #######################################################
-                    # (2) Generate fake images
-                    ######################################################
-                    noise.data.normal_(0, 1)
-                    #netG wird mit zu vielen batches gecalled, sentsent_emb_ und words_embs müssen gesplittet werden
-                    fake_imgs, attention_maps, _, _ = netG(noise, sent_emb, words_embs, mask)
-                    #print("net done")
-                    # G attention
-                    cap_lens_np = cap_lens.cpu().data.numpy()
-                    for j in range(batch_size):
-                        #print(f"j: {j}")
-                        save_name = '%s/%d_s_%d' % (save_dir, i, sorted_indices[j])
-                        for k in range(len(fake_imgs)):
-                            #print(f"k: {k}")
-                            im = fake_imgs[k][j].data.cpu().numpy()
-                            im = (im + 1.0) * 127.5
-                            im = im.astype(np.uint8)
-                            # print('im', im.shape)
-                            im = np.transpose(im, (1, 2, 0))
-                            # print('im', im.shape)
-                            im = Image.fromarray(im)
-                            fullpath = '%s_g%d.png' % (save_name, k)
-                            im.save(fullpath)
+                        captions = Variable(torch.from_numpy(captions))
+                        cap_lens = Variable(torch.from_numpy(cap_lens))
 
-                        for k in range(len(attention_maps)):
-                            #print(f"k2: {k}")
-                            if len(fake_imgs) > 1:
-                                im = fake_imgs[k + 1].detach().cpu()
-                            else:
-                                im = fake_imgs[0].detach().cpu()
-                            attn_maps = attention_maps[k]
-                            att_sze = attn_maps.size(2)
-                            img_set, sentences = \
-                                build_super_images2(im[j].unsqueeze(0),
-                                                    captions[j].unsqueeze(0),
-                                                    [cap_lens_np[j]], self.ixtoword,
-                                                    [attn_maps[j]], att_sze)
-                            if img_set is not None:
-                                im = Image.fromarray(img_set)
-                                fullpath = '%s_a%d.png' % (save_name, k)
+                        captions = captions.cuda()
+                        cap_lens = cap_lens.cuda()
+
+                    for i in range(1):  # 16
+                        #print(f"i: {i}")
+                        with torch.no_grad():
+                            noise = Variable(torch.FloatTensor(batch_size, nz))
+                            noise = noise.cuda()
+                        #print("noise done")
+                        #######################################################
+                        # (1) Extract text embeddings
+                        ######################################################
+                        hidden = text_encoder.init_hidden(batch_size)
+                        #print("textencode1")
+                        # words_embs: batch_size x nef x seq_len
+                        # sent_emb: batch_size x nef
+                        words_embs, sent_emb = text_encoder(captions, cap_lens, hidden)
+                        #print("textencode2")
+                        mask = (captions == 0)
+                        #######################################################
+                        # (2) Generate fake images
+                        ######################################################
+                        noise.data.normal_(0, 1)
+                        #netG wird mit zu vielen batches gecalled, sentsent_emb_ und words_embs müssen gesplittet werden
+                        fake_imgs, attention_maps, _, _ = netG(noise, sent_emb, words_embs, mask)
+                        #print("net done")
+                        # G attention
+                        cap_lens_np = cap_lens.cpu().data.numpy()
+                        for j in range(batch_size):
+                            #print(f"j: {j}")
+                            save_name = '%s/%d_s_%d' % (save_dir, i, sorted_indices[j])
+                            for k in range(len(fake_imgs)):
+                                #print(f"k: {k}")
+                                im = fake_imgs[k][j].data.cpu().numpy()
+                                im = (im + 1.0) * 127.5
+                                im = im.astype(np.uint8)
+                                # print('im', im.shape)
+                                im = np.transpose(im, (1, 2, 0))
+                                # print('im', im.shape)
+                                im = Image.fromarray(im)
+                                fullpath = '%s_g%d.png' % (save_name, k)
                                 im.save(fullpath)
+
+                            for k in range(len(attention_maps)):
+                                #print(f"k2: {k}")
+                                if len(fake_imgs) > 1:
+                                    im = fake_imgs[k + 1].detach().cpu()
+                                else:
+                                    im = fake_imgs[0].detach().cpu()
+                                attn_maps = attention_maps[k]
+                                att_sze = attn_maps.size(2)
+                                img_set, sentences = \
+                                    build_super_images2(im[j].unsqueeze(0),
+                                                        captions[j].unsqueeze(0),
+                                                        [cap_lens_np[j]], self.ixtoword,
+                                                        [attn_maps[j]], att_sze)
+                                if img_set is not None:
+                                    im = Image.fromarray(img_set)
+                                    fullpath = '%s_a%d.png' % (save_name, k)
+                                    im.save(fullpath)
+                except:
+                    print(f"error: {key}")
+                    pass
